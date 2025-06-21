688d8491ccb84fc8fa017c64714abc8047949880
Fabio <fabiocastromorffi@gmail.com>
2025-06-21 10:24:57 -0400

    more

--- queue/Queue_Array.py
+++ queue/Queue_Array.py
@@ -2,17 +2,17 @@
 
 # Fixed Array Implementation
 
-class Queue:
+class QueueList:
     def __init__(self, size:int):
         self.__size = size
-        self.__data = [] * size
+        self.__data = [None] * size
         self.__back = 0
         self.__front = 0
     
     def enqueue(self, value: object):
-        self.__back += 1
-        if self.__back > self.__size:
-            raise 'Max size' 
+        if self.__back == self.__size:
+            raise 'Max size'
+        self.__back += 1 
         self.__data.append(value)
         
     def isEmpty(self):
@@ -21,22 +21,24 @@ class Queue:
         if self.isEmpty():
             raise 'Container is empty'
         else:
+            top = self.first()
             self.__data[self.__front] = None
             self.__front += 1
-            return(self.__data[self.__front])
+            return(top)
     def first(self):
-        print( self.__data[self.__front])
+        return( self.__data[self.__front])
     def __len__(self):
         return self.__back - self.__front
     
 if '__main__' == __name__:
-    queue = Queue(3)
+    queue = QueueList(3)
     queue.enqueue(1)
     queue.enqueue(2)
-    queue.first()
+    print(queue.first())
     #print(queue.dequeue())
     queue.enqueue(2)
-    queue.enqueue(2)
+    print(queue.dequeue())
+    print(len(queue))
 
 
 
--- queue/Queue_CircularArray.py
+++ queue/Queue_CircularArray.py
@@ -1,7 +1,44 @@
-class Queue:
-    def __inti__(self, size):
-        self.__size = size
+class CircularQueue:
+    def __init__(self, maxsize):
+        self.__data = [None] * maxsize
+        self.__size = 0
         self.__front = 0
-        self.__back = 0
-    def isEmpty(self):
-        return self.__back - self.__front
+        self.__max = maxsize
+    def isEmpty(self)->bool:
+        return (self.__size) == 0
+    def first(self) -> object:
+        if self.isEmpty():
+            raise ('Empty')
+        else:
+            return (self.__data[self.__front])
+    def enqueue(self,element:object):
+        if self.__size == self.__max:
+            raise 'Full'
+        self.__data[(self.__size + self.__front) % self.__max] = element
+        self.__size += 1
+    def dequeue(self):
+        if self.isEmpty():
+            raise 'Empty'
+        else:
+            top = self.first()
+            self.__data[self.__front] = None
+            self.__size -= 1
+            self.__front += 1
+            return top
+    def __len__(self):
+        return self.__size
+    
+if '__main__' == __name__:
+    cqueue = CircularQueue(3)
+    cqueue.enqueue(1)
+    cqueue.enqueue(2)
+    print(cqueue.dequeue())
+    cqueue.enqueue(1)
+    print(cqueue.first())
+    print(len(cqueue))
+
+
+        
+
+
+        
