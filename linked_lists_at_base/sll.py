859da7bd300e38b43b6de4484737c24b2b40b94f
Fabio <fabiocastromorffi@gmail.com>
2025-06-29 11:30:23 -0400

    finished sll

--- linked_lists_at_base/sll.py
+++ linked_lists_at_base/sll.py
@@ -25,10 +25,65 @@ class SinglyLinkedList:
         self._tail = new_node
         self._size += 1
     def addAtIndex(self, index, elem):
-        if index < self.__len__():
+        if index == self.__len__():
+            self.insertAtTail(elem)
+            self._size += 1
+        elif index == 0:
+            self.insertAtFront(elem)
+            self._size += 1
+        elif index < self.__len__():
             prev = self._head
             for i in range(index-1):
                 prev = prev._next
-            new_node = self._Node(elem, prev)
-        if index == self.__len__():
-            self.insertAtTail(elem)
+            new_node = self._Node(elem, prev._next)
+            prev._next = new_node
+            self._size += 1
+    def removeAtIndex(self, index):
+        if index == 0 and not self.isEmpty():
+            self._head = self._head._next
+            self._size -= 1
+        elif index == self.__len__() - 1:
+            curr_node = self._head
+            for i in range(index-1):
+                curr_node = curr_node._next
+            self._tail = curr_node
+            curr_node._next = None
+            self._size -= 1
+        elif index < self.__len__() -1:
+            curr_node = self._head
+            prev = None
+            for i in range(index):
+                prev = curr_node
+                curr_node = curr_node._next
+            prev._next= curr_node._next
+            self._size -= 1
+    def __repr__(self):
+        curr_node = self._head 
+        lst = []
+        for _ in range(self.__len__()):
+            lst.append(str(curr_node._element))
+            curr_node = curr_node._next
+        return ' --> '.join(lst)
+    def get(self, index):
+        curr_node = self._head
+        if 0<=index<self.__len__():
+            for i in range(index):
+                curr_node = curr_node._next
+            return curr_node._element
+        
+if '__main__' == __name__:
+    ll = SinglyLinkedList()
+    ll.insertAtTail(3)
+    ll.insertAtHead(2)
+    ll.addAtIndex(1, 9)
+    print(ll)
+    ll.removeAtIndex(1)
+    print(ll)
+    print(ll.get(1))
+    print(len(ll))
+
+            
+    
+                
+
+
